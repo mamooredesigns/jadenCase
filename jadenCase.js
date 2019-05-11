669bd3a051f36af27e7b42f9807760afec2dac51
Mary Alice Moore <mamooredesigns@gmail.com>
2019-05-11 20:24:09 +0000

    changed for loop to a map method

--- jadenCase.js
+++ jadenCase.js
@@ -1,20 +1,12 @@
-
 function jadenCase(text) {
   var text = document.getElementById('original').value;
-
   //getting the text from the input field, set it equal to variable text
 
-    var newText = {
-      //create a variable newText.  newText is equal to the result of calling the split method on text, and calling the map
-      // method on the resulting array
-
-      //return the first letter of each element in the array (x[0])
-      //concatenate it with the rest of the letters in each element (x.slice(1))
-
-    //turn the array back into a sentence
-
-    // document.getElementById("editedText").innerHTML = newText;
-    document.getElementById("editedText").textContent = newText;
-
-    //insert the newText into the document
-};
+  var newText = text.split(" ").map(capitalizeFirstLetter);
+  
+  function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  
+  document.getElementById("editedText").textContent = newText.join(" ");
+}
